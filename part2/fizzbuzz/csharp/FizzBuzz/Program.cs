3e054fd5ba06159a7d8be3c9c2cf70dd94992f23
Sarah Brooks <sarahebrooks12@gmail.com>
2024-07-24 11:36:33 -0400

    review complete

---
+++ part2/fizzbuzz/csharp/FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿// number divisible by three with the word "fizz", and any number divisible by five with the word "buzz", and any number divisible by both three and five with the word "fizzbuzz".
+
+for(int n = 1; n <= 100; n++)
+{
+    if(n % 15 == 0)
+    {
+        Console.WriteLine("fizzbuzz");
+    }
+    else if (n % 3 == 0)
+    {
+        Console.WriteLine("fizz");
+    }
+    else if (n % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+    }
+    else
+    {
+        Console.WriteLine(n);
+    }
+}
---
+++ part2/insulter/csharp/Insulter/Program.cs
@@ -0,0 +1,29 @@
+﻿  List<string> insults = new List<string>() {
+    "You look like what morning breath smells like.",
+    "If you tried to give me cpr I would probably throw myself back under water",
+    "I am not a fan of you",
+    "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
+    "I'd rather walk than be on the same plane as you",
+    "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
+    "Do you have to be so...like that?"
+  };
+
+  //No duplicates - 3 insults - random number
+
+
+
+  List<int> indexes = new List<int>();
+
+//going through the list length and selecting a random number then adding it to the indexes List
+  while(indexes.Count < 3){
+  int randomNumber = new Random().Next(0, insults.Count - 1);  
+    if(!indexes.Contains(randomNumber))
+    {
+        indexes.Add(randomNumber);
+    }
+  }
+
+//looping through indexes to get a random number and calling that specific insult from the insults list based on sub i
+  foreach(int i in indexes){
+    Console.WriteLine(insults[i]);
+  }
